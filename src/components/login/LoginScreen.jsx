172da5d5b163f15e69f236490c8ee88439cfb22b
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-11 10:31:19 -0500

    History properties

--- src/components/login/LoginScreen.jsx
+++ src/components/login/LoginScreen.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ history }) => {
+  /**
+   * History es un propiedad que nos ayuda
+   * a accesader a ciertos metodos de nuestro navegador
+   * o de react.router
+   */
+  const handleLOgin = () => {
+    // push nos redrecciona a una de las rutas
+    history.push("/");
+    // replace remplaza el layout y no permitira volver
+    // a atras al mismo layout
+    history.replace("/");
+  };
+
   return (
-    <div>
+    <div className="container mt-5">
       <h1>LoginScreen</h1>
+      <hr />
+      <button className="btn btn-primary" onClick={handleLOgin}>
+        login
+      </button>
     </div>
   );
 };
