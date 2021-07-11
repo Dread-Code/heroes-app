d87fd0543c022f7480117b001ed4ff034c6cb6ed
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-11 10:23:40 -0500

    Router in router

---
+++ src/routers/DashboardRoutes.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { DcScreen } from "../components/dc/DcScreen";
+import { HeroScreen } from "../components/heroes/HeroScreen";
+import { Marvelscreen } from "../components/marvel/Marvelscreen";
+import { Navbar } from "../components/ui/NavBar";
+
+export const DashboardRoutes = () => {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <Switch>
+          <Route exact path="/heroe/:heroeId" component={HeroScreen} />
+          <Route exact path="/marvel" component={Marvelscreen} />
+          <Route exact path="/dc" component={DcScreen} />
+          <Redirect to="/marvel" />
+        </Switch>
+      </div>
+    </>
+  );
+};
