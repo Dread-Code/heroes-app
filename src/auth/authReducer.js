89c5db18106c76a91444bef56cfe921e227c6620
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-11 23:33:02 -0500

    Auth added

---
+++ src/auth/authReducer.js
@@ -0,0 +1,18 @@
+import { LOGIN, LOGOUT } from "../types/types";
+
+export const authReducer = (state = {}, action) => {
+  switch (action.type) {
+    case LOGIN:
+      return {
+        ...action.payload,
+        logged: true,
+      };
+    case LOGOUT:
+      return {
+        logged: false,
+      };
+
+    default:
+      return state;
+  }
+};
